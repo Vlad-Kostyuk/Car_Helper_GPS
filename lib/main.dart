a33716aca92237e9ce88125fd8ac9edb2ee9adaa
Vlad Kostyuk <vlad.kostyuk24@gmail.com>
2022-04-18 20:09:17 +0300

    [CR-1] finish add gps

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+const LocationSettings locationSettings = LocationSettings(
+  accuracy: LocationAccuracy.bestForNavigation,
+  distanceFilter: 0,
+);
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -27,21 +39,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool button = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Demo Home Page)'),
+        title: const Text('GPS'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
+          children: <Widget>[
+
+          ElevatedButton(
+              onPressed: ()  {
+                StreamSubscription<Position> positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen(
+                        (Position? position) {
+                      log(position == null ? 'Unknown' : '${position.latitude.toString()}, ${position.longitude.toString()}');
+                      log(position == null ? 'Unknown' : position.timestamp.toString());
+                    });
+              },
+              child: null,
+          )
 
           ],
         ),
       ),
     );
   }
+}
+
+
+
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+  return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 }
